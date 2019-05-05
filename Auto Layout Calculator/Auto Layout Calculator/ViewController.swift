a536b3c1621d65bd7c1b6f6a7276020b7c2915c0
tothtomi <tothtomi7@gmail.com>
2019-05-05 08:11:22 +0200

    add stack view exercise

---
+++ Auto Layout Calculator/Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by Tamás Tóth on 2019. 05. 05..
+//  Copyright © 2019. Tamás Tóth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
